8afcf33f85aebf096f2ee135bdf96690abd07567
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-09 09:31:46 -0700

    Add files via upload

---
+++ app/Models/Sale.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'price',
+        'total',
+        'customer_id',
+        'sold_at'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}

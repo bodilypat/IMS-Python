0a91d94a028961bf76a6e345f9f7374dc60c8301
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-09 09:32:04 -0700

    Add files via upload

---
+++ app/Models/Purchase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'cost',
+        'total',
+        'supplier_id',
+        'purchased_at'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+}

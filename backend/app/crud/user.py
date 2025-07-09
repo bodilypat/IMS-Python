8a39636d9c938a4acc6e1256bc2c410f514f8de0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-09 11:26:05 -0700

    Add files via upload

---
+++ backend/app/crud/user.py
@@ -0,0 +1,16 @@
+#backend/app/crud/user.py
+
+from sqlalchemay.orm import Session 
+from app.models.user import User
+from app.core.security import verify_password 
+
+def get_by_username(db: Session, username: str) -> User | None:
+	return db.query(User).filter(User.username == username).first()
+	
+def authenticate(Db: Session, username: str, password: str) -> User | None:
+	user = get_by_username(Db, username)
+		if not or not verify_password(password, user.hashed_password):
+			return None 
+		return user 
+		
+		

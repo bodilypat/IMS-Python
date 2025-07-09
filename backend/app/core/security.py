82d08ba8ab441a5b7301153b5d180b9094a79d25
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-09 12:29:02 -0700

    Add files via upload

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,23 +1,31 @@
 # backend/app/core/security.py
 
-from datetime import datetime, timedelta
-from jose import jwt
-from passlib.context import CryptContext 
+from datetime import datetime, timedelta 
+from typing import Optional
 
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30 
+from jose import jwt 
+from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemas=["bcrypt", deprecated="auto")
+from app.core.config import setting 
 
-def varify_password(plain, hashed):
-	return pwd_context.verify(plain, hashed
+# Password hashing 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_password_hash(password):
-	return pwd_context.hash(password)
+	def verify_password(plian_password: str, hashed_password: str) -> bool:
+		return pwd_context.verify(plain_password, hashed_password)
 	
-def create_access_token(data: dict, expires_timedelta: timedalta | None = None ):
-	to_encode = data.copy()
-	expire = datetime.utcnow() + (expire_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-	to_encode.update("exp": expire})
-	return jwt.endcode(to_encode, SECRET_KEY, algorithm=ALGORITHM) 
+	def get_passwordd_hash(password: str) -> str:
+		return pwd_context.hash(password)
+		
+	def create_access_token(
+		data: dict,
+		expires_delta: Optional[timedelta] = None 
+	) -> str:
+		to_encode = data.copy()
+		expire = datetime.utcnow() + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+		to_encode.update({"exp": expire})
+		encode_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+		return encoded_jwt 
+		
+		
+	

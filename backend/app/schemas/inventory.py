76a603cfcb5b46093e3c569f0429e0df7463eef8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-09 14:24:53 -0700

    Add files via upload

--- backend/app/schemas/inventory.py
+++ backend/app/schemas/inventory.py
@@ -5,17 +5,21 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class InventoryBase(BaseModel):
-	name: str = Field(...)
+	name: str = Field(...,min_length=1)
 	description: Optional[str] = None
-	category: Optional[str] = Field(None,...)
-	unit: str = Field(...)
+	category: Optional[str] = None
+	unit: str = Field(..., min_length=1)
 	is_controlled_substance: bool = False 
 	
 class InventoryCreate(InventoryBase):
 	pass 
 
 class InventoryUpdate(InventoryBase):
-	pass 
+	name: Optional[str] = None
+    description: Optional[str] = None 
+    category: Optional[str] = None 
+    unit: Optional[str] = None
+    is_controlled_subtance: Optional[bool] = None 
 	
 class Inventory(InventoryBase):
 	id: int

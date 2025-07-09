517b9bbcc4139739879a616f8a3ed1cb827e2e41
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-08 21:17:15 -0700

    Add files via upload

---
+++ backend/app/schemas/supplier.py
@@ -0,0 +1,31 @@
+# backend/app/schemas/supplier.py 
+
+fromf pydantic import BaseModel, EmailStr, constr 
+from typing import Optional
+from datetime import datetime
+
+class SupplierBase(BaseModel):
+	name: constr(min_length=1)
+	contact_person: Optional[str] = name 
+	phone: Optional[str] = None 
+	address: Optional[str] = None 
+	
+class SupplierCreate(SupplierBase)
+	pass
+	
+class SupplierUpdate(BaseModel)
+	name: Optional[constr(min_length=1] = None
+	contact_person: Optional[str] = None 
+	phone: Optional[str] = None 
+	email: Optional[EmailStr] = None
+	address: Optional[str] = None
+	
+class Supplier(SupplierBase):
+	id: int
+	created_at: datetime
+	updated_at: Optional[datetime]
+	
+	class Config:
+		orm_mode = True
+		
+		

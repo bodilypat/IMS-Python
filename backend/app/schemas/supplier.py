d6cf13662fa8c10148c2577bafceead16e56d741
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-09 14:29:30 -0700

    Add files via upload

--- backend/app/schemas/supplier.py
+++ backend/app/schemas/supplier.py
@@ -1,22 +1,32 @@
-# backend/app/models/supplier.py
+# backend/app/schemas/supplier.py 
 
-from sqlalchemy import Column, Integer, String, DateTime 
-from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship 
+from pydantic import BaseModel, EmailStr, constr 
+from typing import Optional
+from datetime import datetime
 
-from app.db.base_class import Base 
-
-class Supplier(Base):
-	__tablename__ = "suppliers"
-		id = Column(Integer, primary_key=Ture, index=True)
-		name = Column(string(100), nullable=False, index=True)
-		contact_person = Column(String(100), nullable=True)
-		phone = Column(String(20), nullable=True)
-		email = Column(String(120), nullable=True, unique=True)
-		address = Column(string(255), nullable=True)
+class SupplierBase(BaseModel):
+	name: constr(min_length=1)
+	contact_person: Optional[str] = name 
+	phone: Optional[str] = None 
+    email: Optional[EmailStr] = None
+	address: Optional[str] = None 
+	
+class SupplierCreate(SupplierBase)
+	pass
+	
+class SupplierUpdate(BaseModel)
+	name: Optional[constr(min_length=1] = None
+	contact_person: Optional[str] = None 
+	phone: Optional[str] = None 
+	email: Optional[EmailStr] = None
+	address: Optional[str] = None
+	
+class Supplier(SupplierBase):
+	id: int
+	created_at: datetime
+	updated_at: Optional[datetime] = None
+	
+	class Config:
+		orm_mode = True
 		
-		created_at = Column(DateTime(timezone=True), server_default=func.now())
-		updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-		 
-# Optional reverse relationship to Stock 
-stock = relationship("Stock", back_populates="supplier", cascade="all, delete", layzy="selectin")
+		

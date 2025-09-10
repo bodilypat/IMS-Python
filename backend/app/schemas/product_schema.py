760d7f9b027c8eb557c7ae075d062209abd04b6c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-09-10 11:53:27 -0700

    Add files via upload

--- backend/app/schemas/product_schema.py
+++ backend/app/schemas/product_schema.py
@@ -1,54 +1,46 @@
 #app/schemas/product_schema.py 
 
 from pydantic import BaseModel, constr, condecimal, Field, HttpUrl 
-from typing import Optional
+from typing imort Optional
 from datetime import datetime
 
 # Shared base schema 
 class ProductBase(BaseModel):
-	sku: constr(max_length=1, max_length=100)
+	sku: constr(min_length=1, max_length=100)
 	product_name: constr(min_length=1, max_length=255)
 	description: Optional[str] = None 
 	cost_price: condecimal(max_digits=10, decimal_places=2) = 0.00
 	sale_price: condecimal(max_digits=10, decimal_places=2) = 0.00
 	quantity: int = 0
 	category_id: Optional[int] = None 
-	vendor_id: int 
-	status: constr(strip_whitespace=True) = Field(
-		default="Available",
-		pattern="^(Available|Out of stock|Discontinued)$"
-	) 
+	vendor_id : int 
+	status: constr(strip_whitespace=True, regex=r^(Available|Out of stock|Discontinued)$") = "Available"
 	product_image_url: Optional[HttpUrl] = None 
 	
-# Schema for creation
 class ProductCreate(ProductBase):
-	pass
+	pass 
 	
-# Schema for updates - all fields optional 
-class ProductUpdate(BaseModel):
-	sku: Optional[constr(min_length=1, max_length=100] = None 
+class ProductuUpdate(BaseModel):
+	sku: Optional[constr(min_length=1, max_length=100) = None 
 	product_name: Optional[constr(min_length=1, max_length=255)] = None 
 	description: Optional[str] = None 
-	cost_price: Optional[codecimal(max_digits=10, decimal_places=2)] = None 
-	sale_price: Optional[condecimal(max_digits=10, decimal_places=2)] = None 
-	quantity: Optional[int] = None 
-	category_id: Optional[int] = None 
-	vendor_id: Optional[int] = None 
-	status: Optional[constr[strip_whitespace=True)] = Field(
-		defual=None,
-		pattern="^(Available|Out of stock|Discontinued)$"
-	)
-	product_image_url: Optional[HttpUrl] = None 
-
-# Respose schema
+	cost_price: Optional[condecimal(max_digits=10, decimal_places=2]) = None 
+    sale_price: Optional[condecimal(max_digits=10, decimal_places=2]) = None 
+    quantity: Optional[int] = None 
+    category_id: Optional[int] = None 
+    vendor_id: Optional[int] = None 
+    status: Optional[constr(strip_whitespace=True, regex=r"^(Available|Out of stock|Discontinued)$"] = None 
+    product_image_url: Optional[HttpUrl] = None 
+    
 class ProductResponse(ProductBase):
-	id: int = Field(..., alias="product_id")
-	created_at:datetime
-	updated_at: datetime,
-	deleted_at: Optional[datetime] = None 
+    product_id: int = Field(..., alias="id")
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: Optional[datetime] = None 
+    
+    class Config: 
+        orm_mode = True 
+        allow_population_by_field_name  = True 
+        
+   
 	
-	class Config:
-		orm_mode = True 
-		allow_population_by_field_name = True 
-		
-		

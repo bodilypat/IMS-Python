5035b326b52422a577a3a9a0eb73f9a969b49206
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-11-08 13:29:00 -0800

    Create AuthContext.js

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,27 @@
+//frontend/src/context/AuthContext.js
+
+import React, { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+    const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+    const login = (data) => {
+        setUser(data.user);
+        setToken(data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+    };
+
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        localStorage.clear();
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, token, login, logout }}></AuthContext.Provider>
+    );
+};

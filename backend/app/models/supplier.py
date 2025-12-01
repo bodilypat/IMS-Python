0b3d2f41b4bd77058604191632f1d2a783d16a21
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-12-01 09:51:11 -0800

    Add files via upload

--- backend/app/models/supplier.py
+++ backend/app/models/supplier.py
@@ -1,21 +1,63 @@
 #app/models/supplier.py
 
-from sqlalchemy import Column, Integer, String, Text, DateTime 
-from sqlalchemy.sql import func 
-from app.db import Base 
+from sqlalchemy import ( 
+        Column, 
+        Integer,  
+        String,  
+        Text,
+        Boolean,
+        DateTime,
+        func,
+        CheckConstraint, 
+)
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
 
 class Supplier(Base):
     __tablename__= "suppliers"
 
     supplier_id = Column(Integer, primary_key, index=True)
-    name = Column(String(255), nullable=False)
-    contact_email = Column(String(255), uinique=True, nullable=True)
-    phone = Column(String(20), nullable=True)
+
+    supplier_name = Column(String(255), nullable=False)
+    contact_person = Column(String(255), nullable=True)
+    contact_email = Column(String(255), unique=True, nullable=True)
+    contact_phone = Column(String(20), nullable=True)
+
     address = Column(Text, nullable=True)
 
-# Timestamp 
-    created_at = Columm(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    # ACTIVE/ INACTIVE 
+    status = Column(
+        String(50), 
+        nullable=False,
+        default="active"
+    ) 
+
+    # Timestamps 
+    created_at = Column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        onupdate=func.now(), 
+        nullable=False
+    )
+
+    # Soft Delete
+    is_deleted = Column(Boolean, default=False, nullable=False)
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
+
+    __table_args__ = (
+        CheckConstraint("status IN('Active', 'Inactive')", name="chk_supplier_status") 
+    ),
 
     def __repr__(self):
-        return f"<Supplier id={self.supplier_id}, name='{self.name}'> "
+        return f"<Supplier id={self.supplier_id}, name='{self.supplier_name}'> "
+    
+
+
+
+# Timestamp 
+  
